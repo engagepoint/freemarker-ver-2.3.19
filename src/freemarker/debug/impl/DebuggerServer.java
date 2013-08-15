d75bf5fe1757ca50f2438b6c2a1cd0187fcf0004
leonid.marushevskiy <leonid.marushevskiy@engagepoint.com>
2013-08-15 11:56:11 +0300

    ENT-650: fix insufficient entropy (CWE ID 331)

--- src/freemarker/debug/impl/DebuggerServer.java
+++ src/freemarker/debug/impl/DebuggerServer.java
@@ -62,13 +62,12 @@ import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.util.Arrays;
-import java.util.Random;
 
 import freemarker.debug.Debugger;
 import freemarker.log.Logger;
 import freemarker.template.utility.SecurityUtilities;
 import freemarker.template.utility.UndeclaredThrowableException;
-import com.opensymphony.module.random.Yarrow;
+import java.util.Random;
 
 /**
  * @author Attila Szegedi
@@ -77,8 +76,8 @@ import com.opensymphony.module.random.Yarrow;
 class DebuggerServer
 {
     private static final Logger logger = Logger.getLogger("freemarker.debug.server");
-      
-    private static final Random R = new Yarrow();
+    // TODO: Eventually replace with Yarrow    
+    private static final SecureRandom R = new SecureRandom();
     
     private final byte[] password;
     private final int port;
@@ -117,7 +116,7 @@ class DebuggerServer
             for(;;)
             {
                 Socket s = ss.accept();
-                new Thread(new DebuggerAuthProtocol(s)).start();
+                new Thread(new DebuggerServer.DebuggerAuthProtocol(s)).start();
             }
         }
         catch(IOException e)
